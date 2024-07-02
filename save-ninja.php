88f9a516e9ceb6bbcb11b61060d8d0a87436aadd
yslz16 <yuslizaimas16@gmail.com>
2024-07-02 18:12:12 +0700

    Update save-ninja.php

--- save-ninja.php
+++ save-ninja.php
@@ -1,15 +1,5 @@
 <?php
-// Debug: Periksa metode permintaan
-echo "<pre>";
-echo "Request Method: " . $_SERVER["REQUEST_METHOD"] . "\n";
-echo "</pre>";
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Debug: Print received POST data
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
-
     // Ambil nilai dari form
     $ninjaId = $_POST['ninja-id'];
     $nama = $_POST['nama'];
@@ -33,17 +23,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $data .= "Metode Pembayaran: $metodePembayaran\n";
     $data .= "----------------------------------------\n";
 
-    // Debug: Periksa apakah file dapat ditulis
+    // Simpan data ke dalam file ninja-data.txt
     $file = 'ninja-data.txt';
-    if (is_writable($file)) {
-        if (file_put_contents($file, $data, FILE_APPEND | LOCK_EX)) {
-            echo "Ninja dengan ID $ninjaId berhasil dibeli dan informasi pembelian telah disimpan.<br>";
-        } else {
-            echo "Terjadi kesalahan saat menyimpan data.<br>";
-        }
+    if (file_put_contents($file, $data, FILE_APPEND | LOCK_EX)) {
+        echo "Ninja dengan ID $ninjaId berhasil dibeli dan informasi pembelian telah disimpan.<br>";
     } else {
-        echo "File $file tidak dapat ditulis.<br>";
-        echo "Periksa izin file.<br>";
+        echo "Terjadi kesalahan saat menyimpan data.<br>";
     }
 } else {
     echo "This script only handles POST requests.";
